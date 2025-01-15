1157d49b8fef4dd081d458b6070a0c0fb79fcee9
jessia <sadiajessia@gmail.com>
2025-01-15 17:38:53 +0600

    cart page frontend part done

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,18 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { assets } from '@assets/assets';
 import { Link } from 'react-router-dom';
+import { StoreContext } from '../context/StoreContext';
 
 const Navbar = ({setShowLogin}) => {
   const [menu, setMenu] = useState('menu');
 
+  const {getTotalCartAmount} = useContext(StoreContext);
+
   return (
     <div className="py-5 flex justify-between items-center">
       {/* Logo */}
-      <img 
+      <Link to='/'> <img 
         src={assets.logo} 
         alt="" 
         className="relative w-[83px] rounded-[13px]" 
       />
+      </Link>
 
       {/* Navbar Menu */}
       <ul className="hidden lg:flex gap-5 text-[tomato] text-[18px] list-none">
@@ -49,8 +53,8 @@ const Navbar = ({setShowLogin}) => {
       <div className="flex items-center gap-10 lg:gap-8">
         <img src={assets.search_icon} alt="" className="w-[20px] lg:w-[22px]" />
         <div className="relative">
-          <img src={assets.basket_icon} alt="" className="w-[20px] lg:w-[22px]" />
-          <div className="absolute w-[10px] h-[10px] bg-[tomato] rounded-full -top-[8px] -right-[1px]"></div>
+          <Link to='/cart'> <img src={assets.basket_icon} alt="" className="w-[20px] lg:w-[22px]" /> </Link>
+          <div className={getTotalCartAmount() ? "dot absolute w-[10px] h-[10px] bg-[tomato] rounded-full -top-[8px] -right-[1px]" : ""}></div>
         </div>
         <button onClick= {()=>setShowLogin(true)} 
            className="bg-transparent text-[tomato] border border-[tomato] px-[30px] py-[10px] rounded-full cursor-pointer transition duration-300 hover:bg-[rgb(244,225,230)]">
--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,8 +1,78 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { StoreContext } from '../context/StoreContext'
 
 const Cart = () => {
+
+  const {cartItems, food_list, removeFromCart, getTotalCartAmount} = useContext(StoreContext);
+
   return (
-    <div>Cart</div>
+    <div>
+      <div className="cart mt-[100px]">
+        <div className="cart-items">
+          <div className="cart-items-title items-center text-gray-500 text-[max(1vw, 12px)] grid grid-cols-[1fr_1.5fr_1fr_1fr_1fr_0.5fr] gap-4">
+            <p className='text-[tomato]'>Items</p>
+            <p  className='text-[tomato]'>Title</p>
+            <p  className='text-[tomato]'>Price</p>
+            <p  className='text-[tomato]'>Quantity</p>
+            <p className='text-[tomato]'>Total</p>
+            <p className='text-[tomato]'>Remove</p>
+          </div>
+          <br />
+          <hr />
+
+          {food_list.map((item, index)=>{
+             if(cartItems[item._id]>0)
+             {
+              return(
+                 <div>
+                 <div className="cart-items-title cart-items-item my-[10px] text-black items-center text-gray-500 text-[max(1vw, 12px)] grid grid-cols-[1fr_1.5fr_1fr_1fr_1fr_0.5fr] gap-4">
+                  <img className='w-[50px]' src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>TK. {item.price}</p>
+                  <p>{cartItems[item._id]} </p>
+                  <p>TK. {item.price*cartItems[item._id]}</p>
+                  <p className='cursor-pointer' onClick={()=>removeFromCart(item._id)}>X</p>
+                 </div>
+                 <hr />
+                 </div>
+              )
+             }
+          })}
+        </div>
+        <div className="cart-bottom w-full mt-[80px] flex justify-between gap-[max(12vw, 20px)] md:flex-row max-[750px]:flex-col-reverse"> 
+          <div className="cart-total flex flex-1 flex-col gap-[20px]">
+            <h2>Cart Totals</h2>
+            <div>
+              <div className="cart-total-details flex justify-between text-[#555]">
+                <p>Subtotal</p>
+                <p>TK. {getTotalCartAmount()}</p>
+              </div>
+              <hr className='my-[10 px]' />
+              <div className="cart-total-details flex justify-between text-[#555]">
+                <p>Delivery Fee</p>
+                <p>TK. {25}</p>
+              </div>
+              <hr />
+              <div className="cart-total-details flex justify-between text-[#555]">
+                <b>Total</b>
+                <b>TK. {getTotalCartAmount()+25}</b>
+              </div>
+            </div>
+            <button className='text-white bg-[tomato] w-[max(15vw, 200px)] py-[12 px] rounded-[4 px] cursor-pointer'>Proceed To Checkout</button>
+          </div>
+          <div className="cart-promocode flex flex-1 justify-between max-[750px]:justify-start">
+            <div>
+              <p className='text-[#555]'>If you have a promo code, Enter it here</p>
+              <div className="cart-promocode-input mt-[10 px] flex justify-between items-center bg-[#eaeaea] rounded-[4 px] ">
+                <input type="text" placeholder='promo code' className='bg-transparent outline-none pl-[10px]' />
+                <button className='w-[max(10vw, 150px)] py-[12px] px-[5px] bg-black text-white rounded-[4px] '>Submit</button>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
