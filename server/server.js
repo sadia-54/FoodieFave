5d77e3abc82f99c8d92c2b498f4418c8d232ed12
jessia <sadiajessia@gmail.com>
2025-02-03 13:54:00 +0600

    user register api done

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import {connectDB} from './config/db.js';
 import foodRouter from './routes/foodRoute.js';
+import userRouter from './routes/userRoute.js';
 
 // app config
 const app = express();
@@ -21,6 +22,7 @@ connectDB()
 // api endpoints
 app.use("/api/food", foodRouter)
 app.use("/images", express.static("uploads"))
+app.use("/api/user", userRouter)
 
 app.get('/', (req, res) => {
     res.send("API is running")
