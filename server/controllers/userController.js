954f729eb84951f04d4bd0b906880ebcb419d111
jessia <sadiajessia@gmail.com>
2025-02-03 19:42:14 +0600

    user login api done

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,10 +2,32 @@ import userModal from '../models/userModel.js';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import validator from 'validator'; 
+import userModel from '../models/userModel.js';
 
 
 // login user
 const loginUser = async (req, res) => {
+
+    const {email, password} = req.body
+    try{
+        const user = await userModel.findOne({email})
+        if(!user){
+            return res.json({success: false, message: "User not found!"})
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+        if(!isMatch){
+            return res.json({success: false, message: "Invalid credentials!"})
+        }
+
+        const token = createToken(user._id)
+        res.json({success: true, token})
+
+
+    } catch (error){
+        console.log(error)
+        return res.json({success: false, message: "Error"})
+    }   
 
 }
 
