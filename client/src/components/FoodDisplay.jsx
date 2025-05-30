787960a9da3d116ae078d79778f2bab3882f9419
jessia <sadiajessia@gmail.com>
2025-05-30 13:36:51 +0600

    updated mapping

--- client/src/components/FoodDisplay.jsx
+++ client/src/components/FoodDisplay.jsx
@@ -9,7 +9,7 @@ const FoodDisplay = ({ category }) => {
         <div className="food-display mt-[30px]">
             <h2 className="text-[max(2vw,24px)] font-semibold text-[tomato]">Top Dishes Near You!</h2>
             <div className="grid grid-cols-[repeat(auto-fill,minmax(240px,1fr))] mt-8 gap-x-7 gap-y-12">
-                {food_list.map((item, index) => {
+                {food_list?.map((item, index) => {
                     if (category === 'All' || category === item.category) {
                         return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
                     }
